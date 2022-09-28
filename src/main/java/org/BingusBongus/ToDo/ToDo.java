65bd9a6bf7783a323e89ec288bb47fb8fe1a95c3
colllijo <liamraoulpapi@gmail.com>
2022-09-28 15:14:52 +0200

    Finished all the HTTPTriggers with loads of pain

--- src/main/java/org/BingusBongus/ToDo/ToDo.java
+++ src/main/java/org/BingusBongus/ToDo/ToDo.java
@@ -1,27 +1,25 @@
 package org.BingusBongus.ToDo;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public class ToDo
 {
-    public ToDo(String taskTitle){this(taskTitle, null);}
-    public ToDo(String taskTitle, String taskDescription)
+    private String id;
+    private String createdDate;
+    private String modifiedDate;
+    private String taskDescription;
+    private boolean isComplete;
+
+    public ToDo(String taskDescription)
     {
-        this.id = String.format("%x", UUID.randomUUID().toString());
-        this.createdDate = LocalDateTime.now();
-        this.modifiedDate = LocalDateTime.now();
-        //this.taskTitle = taskTitle;
+        this.id = UUID.randomUUID().toString();
+        this.createdDate = LocalDateTime.now().toString();
+        this.modifiedDate = LocalDateTime.now().toString();
         this.taskDescription = taskDescription;
         this.isComplete = false;
     }
-
-    private String id;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
-    //private String taskTitle;
-    private String taskDescription;
-    private boolean isComplete;
 
     public String getId()
     {
@@ -30,28 +28,59 @@ public class ToDo
 
     public LocalDateTime getCreatedDate()
     {
-        return createdDate;
+        return LocalDateTime.parse(createdDate);
     }
 
     public LocalDateTime getModifiedDate()
     {
-        return modifiedDate;
+        return LocalDateTime.parse(modifiedDate);
     }
 
     public void setModifiedDate(LocalDateTime modifiedDate)
     {
-        this.modifiedDate = modifiedDate;
+        this.modifiedDate = modifiedDate.toString();
     }
 
-//    public String getTaskTitle()
-//    {
-//        return taskTitle;
-//    }
-//
-//    public void setTaskTitle(String taskTitle)
-//    {
-//        this.taskTitle = taskTitle;
-//    }
+    public String getTaskDescription()
+    {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription)
+    {
+        this.taskDescription = taskDescription;
+    }
+
+    public boolean isComplete()
+    {
+        return isComplete;
+    }
+
+    public void setComplete(boolean complete)
+    {
+        isComplete = complete;
+    }
+}
+
+class ToDoCreateModel
+{
+    private String taskDescription;
+
+    public String getTaskDescription()
+    {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription)
+    {
+        this.taskDescription = taskDescription;
+    }
+}
+
+class ToDoUpdateModel
+{
+    private String taskDescription;
+    private boolean isComplete;
 
     public String getTaskDescription()
     {
--- src/main/java/org/BingusBongus/ToDo/ToDoAPI.java
+++ src/main/java/org/BingusBongus/ToDo/ToDoAPI.java
@@ -1,14 +1,15 @@
 package org.BingusBongus.ToDo;
 
+import com.google.gson.Gson;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-import org.BingusBongus.JSON.JSON;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +22,12 @@ public class ToDoAPI
 {
     //Local List of all ToDo's
     static List<ToDo> ToDoList = new ArrayList<>();
+    final public Gson gson = new Gson();
 
     /**
      * Function to crate a new todo.
      * Triggers with an HttpTrigger on api/todo and creates a new todo from the requestbody
      * which is the added to the list of todos
-     *
-     * @see org.BingusBongus.JSON.getParamFromJson()
      *
      * @param request
      * @param context
@@ -45,14 +45,15 @@ public class ToDoAPI
 
         //Get query
         final String query = request.getBody();
-        context.getLogger().info("Java HTTP POST query body " + query);
 
-        ToDo todo = new ToDo(JSON.getPramFromJson(query, "taskDescription"));
+        ToDo todo = new ToDo(gson.fromJson(query, ToDoCreateModel.class).getTaskDescription());
+
         ToDoList.add(todo);
 
+        context.getLogger().info("New Todo JSON: " + gson.toJson(todo));
         context.getLogger().info("Java HTTP POST Request \"CreateToDo\" processed\nNew ToDo has been added to the List");
 
-        return request.createResponseBuilder(HttpStatus.OK).build();
+        return request.createResponseBuilder(HttpStatus.OK).body(todo).build();
     }
 
     /**
@@ -71,7 +72,9 @@ public class ToDoAPI
     {
         context.getLogger().info("Java HTTP GET Request \"GetToDos\" received");
 
-        context.getLogger().info("Java HTTP GET Request \"GetToDos\" processed");
+        if(ToDoList.size() > 0)
+            context.getLogger().info("" + ToDoList.get(0).isComplete());
+
         return request.createResponseBuilder(HttpStatus.OK).body(ToDoList).build();
     }
 
@@ -91,12 +94,27 @@ public class ToDoAPI
         context.getLogger().info("Java HTTP GET Request \"GetToDoById\" with id:${id} received");
 
         //Parse query Parameters
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        final String query = request.getQueryParameters().get("id");
+        final String id = request.getBody().orElse(query);
 
-        ToDo todo = ToDoList.get(0);
+        ToDo todo = null;
+
+        for(ToDo ltodo:ToDoList)
+        {
+            if(ltodo.getId().equals(id))
+                todo = ltodo;
+        }
+
         context.getLogger().info("Java HTTP GET Request \"GetToDoById\" with id:${id} processed");
-        return request.createResponseBuilder(HttpStatus.OK).body(todo).build();
+        if(todo == null)
+        {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).build();
+        }
+        else
+        {
+            return request.createResponseBuilder(HttpStatus.OK).body(todo).build();
+        }
+
     }
 
     /**
@@ -109,17 +127,30 @@ public class ToDoAPI
     public HttpResponseMessage UpdateToDo(
             @HttpTrigger(name = "req", methods = HttpMethod.PUT, authLevel = AuthorizationLevel.ANONYMOUS, route = "todo/{id}")
             HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context
+            final ExecutionContext context,
+            @BindingName("id") String id
     )
     {
-        context.getLogger().info("Java HTTP GET Request \"UpdateToDo\" with id:${id} received");
+        context.getLogger().info("Java HTTP GET Request \"UpdateToDo\" with id:" + id + " received");
 
         //Parse query Parameters
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        final String query = request.getQueryParameters().get("id");
+        final String body = request.getBody().orElse(query);
 
-        context.getLogger().info("Java HTTP GET Request \"UpdateToDo\" with id:${id} processed");
-        return request.createResponseBuilder(HttpStatus.OK).build();
+        ToDo cTodo = null;
+        for(ToDo todo:ToDoList)
+        {
+            if(todo.getId().equals(id))
+            {
+                todo.setComplete(gson.fromJson(body, ToDoUpdateModel.class).isComplete());
+
+                context.getLogger().info("Java HTTP GET Request \"UpdateToDo\" with id:" + id + " processed");
+                return request.createResponseBuilder(HttpStatus.OK).body(todo).build();
+            }
+        }
+
+        context.getLogger().info("Couldn't find " + id + " in ToDoList");
+        return request.createResponseBuilder(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS).build();
     }
 
     /**
@@ -132,16 +163,24 @@ public class ToDoAPI
     public HttpResponseMessage DeleteToDo(
             @HttpTrigger(name = "req", methods = HttpMethod.DELETE, authLevel = AuthorizationLevel.ANONYMOUS, route = "todo/{id}")
             HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context
+            final ExecutionContext context,
+            @BindingName("id") String id
     )
     {
-        context.getLogger().info("Java HTTP GET Request \"DeleteToDo\" with id:${id} received");
+        context.getLogger().info("Java HTTP GET Request \"DeleteToDo\" with id:" + id + " received");
 
         //Parse query Parameters
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
 
-        context.getLogger().info("Java HTTP GET Request \"DeleteToDo\" with id:${id} processed");
-        return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        for(ToDo todo: ToDoList)
+        {
+            if(todo.getId().equals(id))
+            {
+                context.getLogger().info("Java HTTP GET Request \"DeleteToDo\" with id:" + id + " processed");
+                return request.createResponseBuilder(HttpStatus.OK).build();
+            }
+        }
+
+        context.getLogger().info("Couldn't find " + id + " in ToDoList");
+        return request.createResponseBuilder(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS).build();
     }
 }
